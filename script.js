edf020a373d21985795b9e769ad487838fea1488
Nasim Ahmed Bulbul <nasim.bulbul@northsouth.edu>
2022-03-16 22:45:11 +0600

    Second

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-function printPat(n){
-    let x=n;
-    let res="";
+// function printPat(n){
+//     let x=n;
+//     let res="";
     
-    for(let i=n; i>0;i--){
-        for(let j=n; j>0;j--){
-            for(let k=x; k>0;k--){
-                res+=j+" ";
-            }
-        }
-        x--;
-        res+="$";
+//     for(let i=n; i>0;i--){
+//         for(let j=n; j>0;j--){
+//             for(let k=x; k>0;k--){
+//                 res+=j+" ";
+//             }
+//         }
+//         x--;
+//         res+="$";
+//     }
+//     console.log(res);
+//   }
+
+//   printPat(3);
+
+  function getTable(N) {
+    let n=N;
+    let res=[];
+    for(let i=1; i<=10;i++){
+        res[i-1]=(i*n);
     }
     console.log(res);
+    return res;
   }
 
-  printPat(3);
+  getTable(2);
